25bd3e0c5d31b9779064728c3a7f32c6a9e061a8
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2026-03-15 18:10:40 -0300

    Adicionando UserController

---
+++ GestorPro.Api/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using GestorPro.Application.Interfaces.Services;
+using GestorPro.Application.Models.InputModels.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestorPro.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController(IUserService service) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> PostCreateUser([FromBody] CreateUserInputModel inputModel, CancellationToken cancellationToken)
+    {
+        var id = await service.CreateAsync(inputModel, cancellationToken);
+
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        return CreatedAtAction(nameof(PostCreateUser), new { id });
+    }
+}
